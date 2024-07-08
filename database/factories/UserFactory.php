127ef80aae2cf6c4c4712d8369502d154ddfdec4
Aurore  <hoshizora972@gmail.com>
2024-07-08 11:44:10 -0400

    add role table User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            //ajout de la ligne de commande pour generer des role aleatoire
+            'role'=>fake()->randomElement(['admin','user']),
         ];
     }
 
